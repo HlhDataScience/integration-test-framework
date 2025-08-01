6a88f7907106cc60e52509553c8daac90943628a
Héctor López Hidalgo <data_analitics_hlh@protonmail.com>
2025-08-01 16:10:32 +0200

    finished the bases of the abstract class template

--- src/db/transactional_abstractions.py
+++ src/db/transactional_abstractions.py
@@ -3,11 +3,13 @@ from typing import Generic, TypeVar
 
 from pydantic import BaseModel
 from pydantic_settings import BaseSettings
+
 pydantic_config = TypeVar('pydantic_config', bound= BaseModel)
 pydantic_settings_config = TypeVar('pydantic_settings_config', bound= BaseSettings)
+
 class TransactionalFilterInterface(Generic[pydantic_config, pydantic_settings_config], ABC):
     """
-    This ABC it is an interface desing to interact with the APIs
+    This ABC it is an interface design to interact with the APIs
     from transaccional services to obtain information useful for testing.
     It must be subclassed, leaving the implementation details to the
     implementations.
@@ -16,9 +18,13 @@ class TransactionalFilterInterface(Generic[pydantic_config, pydantic_settings_co
     def __init__(
             self,
             config: pydantic_config | pydantic_settings_config,
-            env_config: pydantic_config,
+            env_config: pydantic_settings_config
 
     ):
-        self.config: pydantic_config = config
+        self.config: pydantic_config | pydantic_settings_config = config
+        self.env_config: pydantic_settings_config = env_config
 
     ...
+    @abstractmethod
+    def test(self):
+        raise NotImplementedError()
